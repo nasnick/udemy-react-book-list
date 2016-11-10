4aee73db8f421ede99bf99421f8ba2e33197f00a
nasnick <nas.nick@gmail.com>
2016-11-10 22:42:05 +1300

    added new function to containers/booklist.js

--- src/containers/book-list.js
+++ src/containers/book-list.js
@@ -1,11 +1,17 @@
 //Container is a component that has direct access to state produced by redux
+// Containers are connected to state
+// Redux generated a state object that contained books and mapped them to props to component
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { selectBook } from '../actions/index';
+// Ensures that the action created from action creators flow to all reducers
+import { bindActionCreators } from 'redux';
 
 //removed export statement here because we want to export the container
 //not the component
 class BookList extends Component {
 	renderList() {
+    // Books coming from index.js which come from reducer_books.js
 		return this.props.books.map((book) => {
 			return (
                <li key={book.title} className="list-group-item">{book.title}</li>     
@@ -28,5 +34,10 @@ function mapStateToProps(state) {
   };
 }
 
-export default connect(mapStateToProps)(BookList);
+function mapDispatchToProps(dispatch){
+  //Second argument is the action creator that was imported at the top
+  return bindActionCreators({selectBook: selectBook}, dispatch});
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(BookList);
 
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,6 +2,7 @@ import { combineReducers } from 'redux';
 import BooksReducer from './reducer_books';
 
 const rootReducer = combineReducers({
+	// key of books tied to list of books in reducer_books.js
 	books: BooksReducer
 
 });
